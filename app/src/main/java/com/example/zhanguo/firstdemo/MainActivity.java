cc293223d2a82710b997bd63dbfa55a113068c70
shenxiao.yz <shenxiao.yz@alibaba-inc.com>
2016-09-18 00:23:15 +0800

    测试demo信息修改

--- app/src/main/java/com/example/zhanguo/firstdemo/MainActivity.java
+++ app/src/main/java/com/example/zhanguo/firstdemo/MainActivity.java
@@ -37,7 +37,8 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
+//modify
+        int a=1;
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
